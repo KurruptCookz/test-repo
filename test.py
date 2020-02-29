33e924d9391739706055e89e4d6a2241f3f0c57c
KurruptCookz <46008286+KurruptCookz@users.noreply.github.com>
2020-02-29 11:25:25 -0500

    should fail

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from run import add, subtract, announce
+import sys
 
 announce("running test 'add'")
 result = add(1, 2)
@@ -6,3 +7,4 @@ if result == 3:
   print("passed")
 else:
   print("failed")
+  sys.exit(1)
