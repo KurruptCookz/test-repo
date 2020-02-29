db52d722dc4fcf6f9fc986d8f2a0b3caefbf16d5
KurruptCookz <46008286+KurruptCookz@users.noreply.github.com>
2020-02-29 11:33:24 -0500

    update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,5 @@
 #/bin/bash
 
 echo "$0 is RUNNING"
-
-echo "Do thing here"
-echo "and here"
-
-echo "This is argument #1 => $1"
-
-if [[ "$1" == "andrew" ]]; then echo "Hi Andrew"; fi
-
+python test.py
 echo "$0 is STOPPING"
